63b1a3faf983ba2aeb0cc30caceaf29b02c128f0
Angus-95 <Angus.nicoll@outlook.com>
2026-09-27 00:33:58 +0900

    Update cyclistic_analysis_github_v01.py

--- cyclistic_analysis_github_v01.py
+++ cyclistic_analysis_github_v01.py
@@ -123,6 +123,10 @@ df["ride_length (seconds)"] = ((df["ended_at"] - df["started_at"]).dt.total_seco
 
 df_cleaned = df[(df["rideable_type"] != "classic_bike") | df["end_station_name"].notna()].copy()
 
+#Delete original dataframe after cleaning for memory efficiency.
+
+del df
+
 # Set day/month order
 
 month_order = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
